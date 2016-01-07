3198b06ea0dde74fa874925efa802bbc0273b4f6
lchaney <l2chaney@gmail.com>
2016-01-07 14:05:12 -0700

    Added code to install packages

--- Analysis/01_Load.R
+++ Analysis/01_Load.R
@@ -8,6 +8,16 @@
 #load data
   mgdat <- read.csv("Data/mg_ghsoil.csv")
 
+  
+#install the following packages
+  #packagelist <- c("devtools", "survival", "ggplot2", "grid", "cowplot", 
+  #                 "gridExtra", "scales", "doBy", "tidyr", "dplyr", "rmarkdown")
+  
+  #new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
+  
+  #if(length(new.packages)>0) {install.packages(new.packages, dependencies = TRUE)}
+  
+  
 #load packages
   library(dplyr) #data management
   library(ggplot2) #plotting
