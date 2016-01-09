8059053d9357b7d2c192bca0971dad6822707086
lchaney <l2chaney@gmail.com>
2016-01-09 08:37:51 -0800

    added selection analysis

--- Analysis/03_Func_selection.R
+++ Analysis/03_Func_selection.R
@@ -11,24 +11,98 @@
 
 #????Should I look at maternal line averages?
 
-#relative fitness function
-relfun <- function(x){
-  x / mean(x)
-}
+#create new variables -- realtive fitness
+mgdat <- mgdat %>% select(Genotype, Treatment, one_of(samplevars)) %>%
+  mutate(RelFit = SumFlowers/mean(SumFlowers)) 
 
-#create new variables for all but treatment -- realtive and z score
-mgdat2 <- mgdat[,c(3, 8:11)] %>% mutate_each(funs(z = scale(.), rel = relfun(.)), -Treatment)
+  #%>%
+  #mutate_each(funs(scale), one_of(samplevars)) %>% as.data.frame()
 
-#  mgdat$RelFit <- mgdat$SumFlowers/mean(mgdat$SumFlowers)
+mgdatgeno <- mgdat %>% 
+  group_by(Genotype, Treatment) %>% 
+  summarise_each(funs(mean), -Treatment) %>% ungroup()
 
-#total selection or selection differential/gradient?!?
-  #DOFF
-    #treatment 1
-      with(mgdat2[which(mgdat2$Treatment == 1),], cor.test(SumFlowers_rel, DOFF_z))
-      coef(lm(SumFlowers_rel ~ DOFF_z, data = mgdat2[which(mgdat2$Treatment == 1),]))
-      confint(lm(SumFlowers_rel ~ DOFF_z, data = mgdat2[which(mgdat2$Treatment == 1),]))
-    #treatment 2
-      with(mgdat2[which(mgdat2$Treatment == 2),], cor.test(SumFlowers_rel, DOFF_z))
-      coef(lm(SumFlowers_rel ~ DOFF_z, data = mgdat2[which(mgdat2$Treatment == 2),]))
-      confint(lm(SumFlowers_rel ~ DOFF_z, data = mgdat2[which(mgdat2$Treatment == 2),]))
+###Selection
+
+#Selection Differential, Total Selection, Direct and Indirect Selection (Univariate)
+
+  ts.doff <- lm(RelFit ~ scale(DOFF), data = mgdatgeno)
+#    anova(ts.doff)
+#    summary(ts.doff)
+#      plot(ts.doff) #resids look good
+
+  ts.lvsum <- lm(RelFit ~ scale(LeafSum34), data = mgdatgeno)
+#    anova(ts.lvsum)
+#    summary(ts.lvsum)
+#      plot(ts.lvsum) #resids look good
+    
+  ts.grw <- lm(RelFit ~ scale(HeightRGRC), data = mgdatgeno)
+#    anova(ts.grw)
+#    summary(ts.grw)
+#      plot(ts.grw) #resids look good
+    
+  #not using mean data
+#     ts.doff2 <- lm(RelFit ~ scale(DOFF), data = mgdat)
+#       anova(ts.doff2)
+#       summary(ts.doff2)
+#         plot(ts.doff2) #resids look good
+#         
+#     ts.lvsum2 <- lm(RelFit ~ scale(LeafSum34), data = mgdat)
+#       anova(ts.lvsum2)
+#       summary(ts.lvsum2)
+#         plot(ts.lvsum2) #resids look good
+#         
+#     ts.grw2 <- lm(RelFit ~ scale(HeightRGRC), data = mgdat)
+#       anova(ts.grw2)
+#       summary(ts.grw2)
+#         plot(ts.grw2) #resids look good
       
+
+  
+#Selection Gradient; Direct Selection (mulitvariate analysis)
+
+    ds.mod <- lm(RelFit ~ scale(DOFF) + scale(LeafSum34) + scale(HeightRGRC),
+               data = mgdatgeno)
+#    anova(ds.mod)
+#    summary(ds.mod)
+    
+#not genotype means
+#   ds.mod2 <- lm(RelFit ~ scale(DOFF) + scale(LeafSum34) + scale(HeightRGRC),
+#                  data = mgdat)
+#     anova(ds.mod2)
+#     summary(ds.mod2)
+  
+  #and Quadratic selection gradients
+  dsquad.mod <- lm(RelFit ~ scale(DOFF) + scale(LeafSum34) + scale(HeightRGRC) +
+                     (scale(DOFF):scale(LeafSum34)) + (scale(DOFF):scale(HeightRGRC)) +
+                     (scale(LeafSum34):scale(HeightRGRC)) + I(scale(DOFF)^2) +
+                     I(scale(LeafSum34)^2) + I(scale(HeightRGRC)^2),
+                 data = mgdatgeno)
+#    anova(dsquad.mod)
+#    summary(dsquad.mod) 
+  
+  
+  
+
+#Does the direction and/or intensity of selection differ by treatment (ANCOVA)
+
+#selection differentials  
+#DOFF
+  asd.doff <- lm(RelFit ~ scale(DOFF) + (scale(DOFF):Treatment) + Treatment, data = mgdatgeno)
+    anova(asd.doff)
+    
+#LeafSum34
+  asd.lsum <- lm(RelFit ~ scale(LeafSum34) + (scale(LeafSum34):Treatment) + Treatment, data = mgdatgeno)
+#    anova(asd.lsum)
+    
+#HeightRGRC
+  asd.grw <- lm(RelFit ~ scale(HeightRGRC) + (scale(HeightRGRC):Treatment) + Treatment, data = mgdatgeno)
+#    anova(asd.grw)
+
+#Selection gradients
+
+   asg <- lm(RelFit ~ scale(DOFF) + scale(LeafSum34) + scale(HeightRGRC) +
+                     (scale(DOFF):Treatment) + (scale(LeafSum34):Treatment) +
+                     (scale(HeightRGRC):Treatment) + Treatment,
+                   data = mgdatgeno)
+#  anova(asg)
