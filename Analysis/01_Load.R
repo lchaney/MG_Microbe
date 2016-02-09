4b47659c3311c59a2eb6d36e051b5a3864aa6bc4
Lindsay Chaney <l2chaney@gmail.com>
2016-02-09 13:34:37 -0700

    lattice and colorbrewer packages, source html for contours

--- Analysis/01_Load.R
+++ Analysis/01_Load.R
@@ -11,7 +11,8 @@
   
 #install the following packages
   packagelist <- c("dplyr", "ggplot2", "lme4", "lmerTest", "multcomp", 
-                   "gridExtra", "tidyr", "fields", "wesanderson")
+                   "gridExtra", "tidyr", "fields", "wesanderson",
+                   "lattice", "RColorBrewer")
   
   new.packages <- packagelist[!(packagelist %in% installed.packages()[,"Package"])]
   
@@ -28,4 +29,8 @@
   library(tidyr) #data organization
   library(fields) #surface plot
   library(wesanderson) #colors palletes for plots
+  library(lattice) #used for surface plots
+  library(RColorBrewer) #used for colors of surface plot
   
+#source from html
+  source("http://www.leg.ufpr.br/~walmes/ridiculas/ridiculas_functions.R") #used to add contours to surface plot
