3427c8d9a8d1b5dc4055574fa48f957bf43aab54
Lindsay Chaney <l2chaney@gmail.com>
2016-02-10 12:42:17 -0700

    save figures

--- Analysis/04_Do.R
+++ Analysis/04_Do.R
@@ -17,3 +17,31 @@ source('Analysis/02_Clean.R')
 source('Analysis/03_Func.R')
 
 #==============================================================================================#
+
+#==============================================================================================#		
+#save figures in output	folder
+
+#create Output folder
+if(!file.exists("Output"))
+  dir.create("Output")
+
+#Treatment means of each trait
+ggsave("Output/Treatment_means.png", gebox)
+
+#Difference in selection of flowering time by treatment
+ggsave("Output/Flower_Selection_Treatment.png", ts.doffplot)
+
+#Quadratic plot for Flowering
+ggsave("Output/Flower_Selection_Quadratic.png", doff.quad)
+
+#Surface plot for Flowering and Size
+#ggsave("Output/FS_surface", FSsurface)
+
+#Difference in joint selection of flowering time and growth by treatment
+#ggsave("Output/FG_surface_An", FSsurface.antibiotic)
+#ggsave("Output/FG_surface_In", FSsurface.inoculum)
+#ggsave("Output/FG_surface_Fi", FSsurface.field)
+#ggsave("Output/FG_surface_Ac", FSsurface.autoclave)
+
+#==============================================================================================#		
+
