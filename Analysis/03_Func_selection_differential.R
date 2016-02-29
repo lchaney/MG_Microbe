63e6ddce897ba1db4043ef9a070052217f9eb72e
Lindsay Chaney <l2chaney@gmail.com>
2016-02-29 10:43:41 -0700

    moved treatment differences to new script

--- Analysis/03_Func_selection_differential.R
+++ Analysis/03_Func_selection_differential.R
@@ -22,29 +22,3 @@ ts.lvsum <- lm(RelFit ~ LeafSum34, data = mgdat2)
 ts.grw <- lm(RelFit ~ HeightRGRC, data = mgdat2)
 #    anova(ts.grw)
 #    summary(ts.grw)
-
-#Does the direction and/or intensity of selection differ by treatment (ANCOVA)
-#selection differentials - ANCOVA  
-#DOFF
-asd.doff <- lm(RelFit ~ DOFF + DOFF:Treatment + Treatment, data = mgdat2)
-anova(asd.doff)
-
-ts.doff.treat <- lapply(unique(mgdat2$Treatment), function(t){
-  summary(lm(RelFit ~ DOFF, data = mgdat2[ which(mgdat2$Treatment == t), ]))
-})  
-
-#LeafSum34
-asd.lsum <- lm(RelFit ~ LeafSum34 + LeafSum34:Treatment + Treatment, data = mgdat2)
-#    anova(asd.lsum)
-
-ts.lvsum.treat <- lapply(unique(mgdat2$Treatment), function(t){
-  summary(lm(RelFit ~ LeafSum34, data = mgdat2[ which(mgdat2$Treatment == t), ]))
-})    
-
-#HeightRGRC
-asd.grw <- lm(RelFit ~ HeightRGRC + HeightRGRC:Treatment + Treatment, data = mgdat2)
-#    anova(asd.grw)
-
-ts.grw.treat <- lapply(unique(mgdat2$Treatment), function(t){
-  summary(lm(RelFit ~ HeightRGRC, data = mgdat2[ which(mgdat2$Treatment == t), ]))
-})      
