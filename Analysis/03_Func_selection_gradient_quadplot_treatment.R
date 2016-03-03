6b940f14138852eabc9b4c5b801825ee3fe0ab2c
Lindsay Chaney <l2chaney@gmail.com>
2016-03-03 12:48:55 -0700

    focus on two treatments

--- Analysis/03_Func_selection_gradient_quadplot_treatment.R
+++ Analysis/03_Func_selection_gradient_quadplot_treatment.R
@@ -7,24 +7,6 @@
 
 
 #is this true stabilizing/disruptive selection with an intermediate optimum for fitness  
-#Antibiotic
-mgdat2a <- mgdat2[ which(mgdat2$Treatment == "Antibiotic"), ]
-
-  fita <- lm(RelFit ~ as.numeric(DOFF) + I(as.numeric(DOFF^2)), data = mgdat2a)
-  prda <- data.frame(DOFF = seq(from = range(mgdat2a$DOFF)[1], to = range(mgdat2a$DOFF)[2], length.out = 100))
-  erra <- predict(fita, newdata = prda, se.fit = TRUE)
-
-    prda$lci <- erra$fit - 1.96 * erra$se.fit
-    prda$fit <- erra$fit
-    prda$uci <- erra$fit + 1.96 * erra$se.fit
-
-doff.quad.Antibiotic <- ggplot(data = prda, aes(x = DOFF, y = fit)) +
-  theme_bw() +
-  geom_line() +
-  geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest")[1], stat = "identity") +
-  geom_point(data = mgdat2a, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest")[1]) + 
-  labs(title = "Antibiotic", x = "Flowering Time", y = "Relative Fitness")
-
 #Inoculum
 mgdat2i <- mgdat2[ which(mgdat2$Treatment == "Inoculum"), ]
 
@@ -42,26 +24,6 @@ doff.quad.Inoculum <- ggplot(data = prdi, aes(x = DOFF, y = fit)) +
   geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest")[2], stat = "identity") +
   geom_point(data = mgdat2i, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest")[2]) + 
   labs(title = "Inoculum", x = "Flowering Time", y = "Relative Fitness")
-
-
-#Field
-mgdat2f <- mgdat2[ which(mgdat2$Treatment == "Field"), ]
-
-fitf <- lm(RelFit ~ as.numeric(DOFF) + I(as.numeric(DOFF^2)), data = mgdat2f)
-prdf <- data.frame(DOFF = seq(from = range(mgdat2f$DOFF)[1], to = range(mgdat2f$DOFF)[2], length.out = 100))
-erraf <- predict(fitf, newdata = prdf, se.fit = TRUE)
-
-prdf$lci <- erraf$fit - 1.96 * erraf$se.fit
-prdf$fit <- erraf$fit
-prdf$uci <- erraf$fit + 1.96 * erraf$se.fit
-
-doff.quad.Field <- ggplot(data = prdf, aes(x = DOFF, y = fit)) +
-  theme_bw() +
-  geom_line() +
-  geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest")[3], stat = "identity") +
-  geom_point(data = mgdat2f, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest")[3]) + 
-  labs(title = "Field", x = "Flowering Time", y = "Relative Fitness")
-
 
 #Autoclave
 mgdat2ac <- mgdat2[ which(mgdat2$Treatment == "Autoclave"), ]
@@ -83,4 +45,4 @@ doff.quad.Autoclave <- ggplot(data = prdac, aes(x = DOFF, y = fit)) +
 
 
 
-#grid.arrange(arrangeGrob(doff.quad.Antibiotic, doff.quad.Inoculum, doff.quad.Field, doff.quad.Autoclave, nrow = 1))
+#grid.arrange(arrangeGrob(doff.quad.Inoculum, doff.quad.Autoclave, nrow = 1))
--- Analysis/03_Func_selection_gradient_surfaceplot_treatment.R
+++ Analysis/03_Func_selection_gradient_surfaceplot_treatment.R
@@ -6,50 +6,6 @@
 #==============================================================================================#
 
 
-#graph the surface plot for flowering day and size
-#set colors
-#colr <- brewer.pal(11, "Spectral")
-#colr <- colorRampPalette(colr, space="rgb")
-
-#data subset by treatment
-#mgdat2a -- antibiotic
-#mgdat2i -- inoculum
-#mgdat2f -- field
-#mgdat2ac -- autoclave
-
-#prepare data -- expand to extrapolate across the whole surface
-
-#anitibiotic
-mg.loessa = loess(RelFit ~ DOFF*HeightRGRC, data = mgdat2a, degree = 2, span = 0.25)
-
-expand.data = expand.grid(list(DOFF = seq(from = range(mgdat2a$DOFF)[1], 
-                                         to = range(mgdat2a$DOFF)[2], length.out = 100), 
-                               HeightRGRC = seq(from = range(mgdat2a$HeightRGRC)[1], 
-                                              to = range(mgdat2a$HeightRGRC)[2], length.out = 100)
-))
-
-z = predict(mg.loessa, newdata = expand.data)
-expand.data$z = as.numeric(z)
-
-#surface plot
-FSsurface.antibiotic <- wireframe(z ~ DOFF*HeightRGRC, data = expand.data,
-                       scales=list(arrows=TRUE),
-                       zlab=list("Relative Fitness", rot = 95),
-                       xlab=list("Flowering Day"), 
-                       ylab=list("Growth"),
-                       colorkey = TRUE,
-                       #screen = list(z = -20, x = -60),
-                       col.regions = colr(100), 
-                       col="gray50", 
-                       col.contour=1,
-                       panel.3d.wireframe="panel.3d.contour", 
-                       type="on",
-                       drape=TRUE
-)
-
-
-
-
 #inoculum
 mg.loessi = loess(RelFit ~ DOFF*HeightRGRC, data = mgdat2i, degree = 2, span = 0.25)
 
@@ -79,38 +35,6 @@ FSsurface.inoculum <- wireframe(z ~ DOFF*HeightRGRC, data = expand.dati,
 )
 
 
-
-
-#field
-mg.loessf = loess(RelFit ~ DOFF*HeightRGRC, data = mgdat2f, degree = 2, span = 0.25)
-
-expand.datf = expand.grid(list(DOFF = seq(from = range(mgdat2f$DOFF)[1], 
-                                          to = range(mgdat2f$DOFF)[2], length.out = 100), 
-                               HeightRGRC = seq(from = range(mgdat2f$HeightRGRC)[1], 
-                                                to = range(mgdat2f$HeightRGRC)[2], length.out = 100)
-))
-
-z = predict(mg.loessf, newdata = expand.datf)
-expand.datf$z = as.numeric(z)
-
-#surface plot
-FSsurface.field <- wireframe(z ~ DOFF*HeightRGRC, data = expand.datf,
-                                scales=list(arrows=TRUE),
-                                zlab=list("Relative Fitness", rot=95),
-                                xlab=list("Flowering Day"), 
-                                ylab=list("Growth"),
-                                colorkey = TRUE,
-                                #screen = list(z = -120, x = -80),
-                                col.regions = colr(100), 
-                                col="gray50", 
-                                col.contour=1,
-                                panel.3d.wireframe="panel.3d.contour", 
-                                type="on",
-                                drape=TRUE
-)
-
-
-
 #autoclave
 mg.loessac = loess(RelFit ~ DOFF*HeightRGRC, data = mgdat2ac, degree = 2, span = 0.25)
 
@@ -121,7 +45,7 @@ expand.datac = expand.grid(list(DOFF = seq(from = range(mgdat2ac$DOFF)[1],
 ))
 
 z = predict(mg.loessac, newdata = expand.datac)
-expand.datf$z = as.numeric(z)
+expand.datac$z = as.numeric(z)
 
 #surface plot
 FSsurface.autoclave <- wireframe(z ~ DOFF*HeightRGRC, data = expand.datac,
@@ -141,4 +65,4 @@ FSsurface.autoclave <- wireframe(z ~ DOFF*HeightRGRC, data = expand.datac,
 
 
 
-#grid.arrange(arrangeGrob(FSsurface.antibiotic, FSsurface.inoculum, FSsurface.field, FSsurface.autoclave, nrow = 1))
+#grid.arrange(arrangeGrob(FSsurface.inoculum, FSsurface.autoclave, nrow = 1))
