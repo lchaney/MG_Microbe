183fcf1a35244762f6b3d4865a2e3e40c1cf9308
Lindsay Chaney <l2chaney@gmail.com>
2016-03-18 11:27:53 -0600

    Added points to contour plot

--- Analysis/03_Func_selection_gradient_surfaceplot_treatment.R
+++ Analysis/03_Func_selection_gradient_surfaceplot_treatment.R
@@ -11,13 +11,18 @@ pal <- wes_palette("Zissou", 100, type = "continuous") #set color pallete
 xFGi <- cbind(mgdat2i$HeightRGRC, mgdat2i$DOFF)
 outFGi <- Tps(xFGi, mgdat2i$RelFit)
 
-FG.surface.In <- surface(outFGi, type="p", xlab="Growth", ylab="Flowering Day", zlab="Fitness", add.legend=FALSE, col= pal, border = NA)
-FG.countour.In <- surface(outFGi, type="C", xlab="Growth", ylab="Flowering Day", add.legend = TRUE, col = pal)
-
+  #surface plot
+  FG.surface.In <- surface(outFGi, type="p", xlab="Growth", ylab="Flowering Day", zlab="Fitness", add.legend=FALSE, col= pal, border = NA)
+  #contour plot
+  FG.contour.In <- surface(outFGi, type="C", xlab="Growth", ylab="Flowering Day", add.legend = TRUE, col = pal)
+  points(xFGi, pch = 2, col = "gray20", cex = .8)
 
 #autoclave
 xFGac <- cbind(mgdat2ac$HeightRGRC, mgdat2ac$DOFF)
 outFGac <- Tps(xFGac, mgdat2ac$RelFit)
 
-FG.surface.Au <- surface(outFGac, type="p", xlab="Growth", ylab="Flowering Day", zlab="RelFit", add.legend = FALSE, col= pal, border = NA)
-FG.countour.Au <- surface(outFGac, type="C", xlab="Growth", ylab="Flowering Day", zlab="RelFit", add.legend = TRUE, col = pal)
+  #surface plot
+  FG.surface.Au <- surface(outFGac, type="p", xlab="Growth", ylab="Flowering Day", zlab="RelFit", add.legend = FALSE, col= pal, border = NA)
+  #contour plot
+  FG.contour.Au <- surface(outFGac, type="C", xlab="Growth", ylab="Flowering Day", zlab="RelFit", add.legend = TRUE, col = pal)
+  points(xFGac, pch = 2, col = "gray20", cex = .8)
