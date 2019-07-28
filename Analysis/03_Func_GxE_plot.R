ecf38d6fda31db3ed4f9a21888fb20193ccefc95
Lindsay Chaney <l2chaney@gmail.com>
2019-07-28 09:15:22 -0500

    Updated R version and header
    Removed plotting options.
    Updated switch to strip.position

--- Analysis/03_Func_GxE_plot.R
+++ Analysis/03_Func_GxE_plot.R
@@ -1,8 +1,9 @@
 #==============================================================================================#
-# Script created by Lindsay Chaney 2015 - lchaney@byu.edu
-# Script created in version R 3.2.3 
-# This script is used for the Chaney et al 2016 Morning glory microbe paper
-# This function examines plots trait differences by treatment
+# Script created by Lindsay Chaney 2019 - lindsay.chaney@snow.edu
+# Script created in version R 3.6.1
+# This script is used to LOAD data and packages needed for 
+# Chaney & Buacom 2019 "The soil microbial community alters patterns of 
+#selection on flowering time and fitness related traits in Ipomoea purpurea"
 #==============================================================================================#
 
 
@@ -24,7 +25,6 @@ dat.m2 <- mgdat %>%
          lower = mean(value) - std(value)) # lower limit for error bar
 
 
-#Plot options
 #boxplot
 gebox <- ggplot(data = dat.m, #use melted data 
                 aes(x=Treatment, y = value, fill = Treatment)) + #variables
@@ -33,86 +33,9 @@ gebox <- ggplot(data = dat.m, #use melted data
   facet_wrap( ~ variable, #facet wrap
               nrow = 1, scales = "free", #one row for facet wrap
               labeller = as_labeller(var_names), #label names
-              switch = "x") + #facet label on bottom
+              strip.position = "bottom") + #facet label on bottom
   scale_fill_manual(values = wes_palette("GrandBudapest1")) + #color pallete
   labs(x = NULL,  y = NULL) + #no x or y labels
-  theme(axis.ticks = element_blank(), #removes x tick labels
-        axis.text.x = element_blank(), #removes x tick labels
-        #axis.line.x=element_blank(), #turns off x axis line
-        axis.line = element_line(size = .2)) #adds faint line to axis
-
-
-#Violin plot with mean
-gevio <- ggplot(dat.m, #use melted data
-                aes(x = variable, y = value, fill = Treatment)) + #variables
-  geom_violin(adjust = .6) + #violin plot, adjusts makes it more shapely
-  theme_minimal() + #minimal theme
-  scale_fill_manual(values = wes_palette("GrandBudapest1")) + #color pallete
-  stat_summary(fun.y = mean, #add mean point to the plot
-               geom = "point", 
-               position = position_dodge(width = .9), #seperate by treatment
-               size = 10, #size of shape
-               shape = "-", #shape
-               show.legend = FALSE) + #not in legend
-  labs(x = NULL, y = NULL) + #no x or y label
-  facet_wrap( ~ variable, #facet wrap
-              nrow = 1, #one row
-              scales = "free", #scales differ by trait
-              labeller = as_labeller(var_names), #label names
-              switch = "x") + #facet label on bottom
-  theme(axis.ticks = element_blank(), #removes x tick labels
-        axis.text.x = element_blank(), #removes x tick labels
-        #axis.line.x=element_blank(), #turns off x axis line
-        axis.line = element_line(size = .2)) #adds faint line to axis
-
-
-#Jitter dot plot with mean
-gedot <- ggplot(dat.m, 
-                aes(x = variable, y = value, fill = Treatment, colour = Treatment)) +
-  stat_summary(fun.y = mean, 
-               geom = "point", 
-               position = position_dodge(width = .5),
-               size = 30, 
-               shape = "-", 
-               show.legend = FALSE, 
-               alpha = .7) + # plot mean as "-"
-  geom_jitter(position = position_jitterdodge(jitter.width = .3, 
-                                              jitter.height = .1,
-                                              dodge.width = .5),
-              size = 1, 
-              alpha = .85) + #plot individual points jittered
-  scale_color_manual(values = wes_palette("GrandBudapest1")) + #colors
-  theme_minimal() + #minimal theme    
-  labs(x = NULL, y = NULL) + #no x or y label
-  facet_wrap( ~ variable, #facet wrap
-              nrow = 1, #one row
-              scales = "free", #scales differ by trait
-              labeller = as_labeller(var_names), #label names
-              switch = "x") + #facet label on bottom
-  theme(axis.ticks = element_blank(), #removes x tick labels
-        axis.text.x = element_blank(), #removes x tick labels
-        #axis.line.x=element_blank(), #turns off x axis line
-        axis.line = element_line(size = .2)) #adds faint line to axis
-
-
-#Barplot with mean and Std
-gebar <- ggplot(dat.m2, 
-                aes(x = variable, y = value, fill = Treatment)) +
-  stat_summary(fun.y = mean, 
-               geom = "bar", 
-               position = position_dodge(width = .9),
-               size = 3) +
-  geom_errorbar(aes(ymin = lower, ymax = upper),
-                width = .1,                    # Width of the error bars
-                position = position_dodge(.9)) +
-  scale_fill_manual(values = wes_palette("GrandBudapest1")) + #colors
-  theme_minimal() + #minimal theme    
-  labs(x = NULL, y = NULL) + #no x or y label
-  facet_wrap( ~ variable, #facet wrap
-              nrow = 1, #one row
-              scales = "free", #scales differ by trait
-              labeller = as_labeller(var_names), #label names
-              switch = "x") + #facet label on bottom
   theme(axis.ticks = element_blank(), #removes x tick labels
         axis.text.x = element_blank(), #removes x tick labels
         #axis.line.x=element_blank(), #turns off x axis line
