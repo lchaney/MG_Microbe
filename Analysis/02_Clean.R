588cba2afb4e614f01cb5da39c14058529633e35
Lindsay Chaney <l2chaney@gmail.com>
2019-07-28 09:09:11 -0500

    Updated R version and header

--- Analysis/02_Clean.R
+++ Analysis/02_Clean.R
@@ -1,7 +1,9 @@
 #==============================================================================================#
-# Script created by Lindsay Chaney 2015 - lchaney@byu.edu
-# Script created in version R 3.2.1 
-# This script is used to CLEAN data needed for the Chaney et al 2016 Morning glory microbe paper
+# Script created by Lindsay Chaney 2019 - lindsay.chaney@snow.edu
+# Script created in version R 3.6.1
+# This script is used to LOAD data and packages needed for 
+# Chaney & Buacom 2019 "The soil microbial community alters patterns of 
+#selection on flowering time and fitness related traits in Ipomoea purpurea"
 #==============================================================================================#
 
 #clean data
