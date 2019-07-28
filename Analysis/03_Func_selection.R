8eff9dd6c8278c34a3ddde1ca404b41655f482df
Lindsay Chaney <l2chaney@gmail.com>
2019-07-28 09:16:39 -0500

    updated R version and header

--- Analysis/03_Func_selection.R
+++ Analysis/03_Func_selection.R
@@ -1,8 +1,9 @@
 #==============================================================================================#
-# Script created by Lindsay Chaney 2015 - lchaney@byu.edu
-# Script created in version R 3.2.1 
-# This script is used for the Chaney et al 2016 Morning glory microbe paper
-# This function examines selection differences by treatment on different traits
+# Script created by Lindsay Chaney 2019 - lindsay.chaney@snow.edu
+# Script created in version R 3.6.1
+# This script is used to LOAD data and packages needed for 
+# Chaney & Buacom 2019 "The soil microbial community alters patterns of 
+#selection on flowering time and fitness related traits in Ipomoea purpurea"
 #==============================================================================================#
 
 #SumFlowers will be used as proxy for fitness
