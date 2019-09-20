2cfcd3cd430811b5418917043591e2c1a9c5c0f3
Lindsay Chaney <l2chaney@gmail.com>
2019-09-20 06:30:45 -0600

    Update 03_Func_selection_gradient_quadplot_treatment.R
    Figure labels

--- Analysis/03_Func_selection_gradient_quadplot_treatment.R
+++ Analysis/03_Func_selection_gradient_quadplot_treatment.R
@@ -22,11 +22,11 @@ prdi$uci <- errai$fit + 1.96 * errai$se.fit
 doff.quad.Inoculum <- ggplot(data = prdi, aes(x = DOFF, y = fit)) +
   theme_bw() +
   geom_line() +
-  geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest1")[2], stat = "identity") +
-  geom_point(data = mgdat2i, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest1")[2]) + 
+  geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest")[2], stat = "identity") +
+  geom_point(data = mgdat2i, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest")[2]) + 
   scale_x_continuous(limits = c(-3.5, 2.5)) +
   scale_y_continuous(limits = c(0, 3.5)) +
-  labs(title = "Autoclave Inoculum", x = "Flowering Time", y = "Relative Fitness")
+  labs(title = "Complex", x = "Flowering Day", y = "Relative Fitness")
 
 #Autoclave
 mgdat2ac <- mgdat2[ which(mgdat2$Treatment == "Autoclave"), ]
@@ -42,11 +42,11 @@ prdac$uci <- errac$fit + 1.96 * errac$se.fit
 doff.quad.Autoclave <- ggplot(data = prdac, aes(x = DOFF, y = fit)) +
   theme_bw() +
   geom_line() +
-  geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest1")[4], stat = "identity") +
-  geom_point(data = mgdat2ac, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest1")[4]) + 
+  geom_smooth(aes(ymin = lci, ymax = uci), color = wes_palette("GrandBudapest")[4], stat = "identity") +
+  geom_point(data = mgdat2ac, aes(x = DOFF, y = RelFit), color = wes_palette("GrandBudapest")[4]) + 
   scale_x_continuous(limits = c(-3.5, 2.5)) +
   scale_y_continuous(limits = c(0, 3.5)) +
-  labs(title = "Autoclave", x = "Flowering Time", y = "Relative Fitness")
+  labs(title = "Simple", x = "Flowering Day", y = "Relative Fitness")
 
 
 
